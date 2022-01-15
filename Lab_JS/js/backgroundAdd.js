ccbcb9c00b1aa630840376b4980a0a99a69e3dd9
AlexandraLevacheva <67224892+alexandralevacheva@users.noreply.github.com>
2022-01-15 10:31:41 +0300

    Add files via upload

---
+++ Lab_JS/js/backgroundAdd.js
@@ -0,0 +1,5 @@
+let ReplaceBackground = function() {
+    document.body.style.backgroundImage = 'url(https://mir-s3-cdn-cf.behance.net/project_modules/fs/4eba9c129042145.6163027e42968.jpg)';
+}
+
+document.addEventListener("DOMContentLoaded", ReplaceBackground);
